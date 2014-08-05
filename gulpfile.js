db4d702bc2375515fd4895469e441e3d2344852a
Kent C. Dodds <kent@doddsfamily.us>
2014-08-05 13:52:14 -0600

    Step 4 - Add gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
+var deploy = require('gulp-gh-pages');
+
 gulp.task('build', function() {
   gulp.src('src/*.js').pipe(gulp.dest('dist'));
 
@@ -11,4 +13,10 @@ gulp.task('build', function() {
     .pipe(rename('marvel-random.min.js'))
     .pipe(gulp.dest('dist'));
 
+});
+
+gulp.task('deploy', function () {
+  gulp.src('./demo/**/*')
+    .pipe(deploy())
+    .pipe(gulp.dest('.tmp'));
 });
